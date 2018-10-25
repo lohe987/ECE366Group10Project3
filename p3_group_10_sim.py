36c5d74b8fb51807530f67fdb755c83b6d4c82b1
Amaan Baiyat <abaiya2@uic.edu>
2018-10-25 17:13:22 -0500

    Added file_to_array function, and fixed typo in dmem file names

--- p3_group_10_sim.py
+++ p3_group_10_sim.py
@@ -1,13 +1,40 @@
 print("ECE 366 Group 10 Project 3 ISA Simulator")
 
 
+# file_to_array: Reads each line of a file into an array as elements
+# Inputs: file - file that is to be parsed
+# Outputs: return_array - array that contains each line
+# of the file as an array element
+def file_to_array(file):
+    return_array = []
+    # Reads in each line into the next array element, and
+    # removes the newline character
+    for line in file:
+        return_array.append(line.rstrip())
+    return return_array
+
+
+# simulator: Simulates the ISA
+# Inputs:   program_name - name of the program that gets printed
+#           instr_mem_file - name of the instruction file that
+#               will be read
+#           data_mem_file - name of the data memory file
+#               that will be read and written to
 def simulator(program_name, instr_mem_file, data_mem_file):
     # Initialize pc and register array
     pc = 0
     reg_arr = [0, 0, 0, 0]
+
     print(program_name)
-    print(instr_mem_file)
-    print(data_mem_file)
+
+    instr_mem_input = open(instr_mem_file, "r")
+    data_mem_input = open(data_mem_file, "r")
+
+    instr_mem = file_to_array(instr_mem_input)
+    data_mem = file_to_array(data_mem_input)
+
+    print(instr_mem)
+    print(data_mem)
 
 
 simulator("Program 1 : Modular Exponentiation",
