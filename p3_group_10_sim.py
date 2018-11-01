e5081fc6805e61c6a21f8db0fdcd143302b69ecd
Justino Almazan <jalmaz9@uic.edu>
2018-11-01 08:44:03 -0500

    Added more comments to simulator

--- p3_group_10_sim.py
+++ p3_group_10_sim.py
@@ -34,7 +34,7 @@ def execute_operation(opcode, data_mem, reg_arr, special_reg_arr, pc, branch):
         print("ADDI")
         rd = int(opcode[4:6], 2)
         imm = int(opcode[6:8], 2)
-        imm_values = [0, 1, -2, -1]
+        imm_values = [0, 1, -2, -1]#These are the only immediate values allowed to be added to registers
         reg_arr[rd] += imm_values[imm]
         pc += 1
     elif opcode[1:4] == "010":
@@ -109,28 +109,27 @@ def execute_operation(opcode, data_mem, reg_arr, special_reg_arr, pc, branch):
         else:
             a = '{0:016b}'.format(reg_arr[rd])
         print("A")
-        print(a)#this is meant for debugging to show us what's going on during the runtime of this simulator
+        print(a)#this if else is meant for debugging to show us what's going on during the runtime of this simulator
         data_mem[reg_arr[rs]] = a
         pc += 1
     elif opcode[1:6] == "11000":
         # left shift logic instruction
         print("LSL")
         rd = int(opcode[6:8], 2)
-        # multiply the register by 2
         dec_value = reg_arr[rd]
         a = ""
         if dec_value < 0:
             print("NEGATIVE LEFT SHIFT")
-            pos_value = 0 - dec_value
-            a = 0b1111111111111111 - pos_value + 1
-            a = str('{0:016b}'.format(a))
+            pos_value = 0 - dec_value#flip rd value
+            a = 0b1111111111111111 - pos_value + 1#obtain 2's C for debugging
+            a = str('{0:016b}'.format(a))#shows us base 2
             print("A: ", a)
-        else:
+        else:#This instruction was a pain because it would shift the decimal value from -20000 to -40000 which is technically out of range so in this if else statement you see where we got the idea for our method of debugging
             a = str('{0:016b}'.format(reg_arr[rd]))
         print("BINARY VALUE:  ", a)
-        bin_value = '{0:016b}'.format(int(a))
-        new_bin_str = a[1:16] + "0"
-        #print("bin_value:     ", str(bin_value))
+        bin_value = '{0:016b}'.format(int(a))#We adjust the binary value to accept a zero being added to it
+        new_bin_str = a[1:16] + "0"#since we begin writing from [1:16] we can shift everything over to 0:15 then add the zero to LSB 
+        #print("bin_value:     ", str(bin_value)) --ignore this line, it didn't work
         print("new_bin_value: ", new_bin_str)
         if new_bin_str[0] == "1":
             value = 0b1111111111111111 - int(new_bin_str, 2) + 1
@@ -139,26 +138,25 @@ def execute_operation(opcode, data_mem, reg_arr, special_reg_arr, pc, branch):
             reg_arr[rd] = int(new_bin_str, 2)
         print("ORIGINAL VALUE: ", dec_value)
         print("NEW VALUE: ", reg_arr[rd])
-        #reg_arr[rd] = (reg_arr[rd] * 2) % 33678
+        #reg_arr[rd] = (reg_arr[rd] * 2) % 33678 --ignore this line, it was 1/4 of our attempts at computing logic shift left
         pc += 1
     elif opcode[1:6] == "11001":
         # nxor instruction
         print("NXOR")
         rd = int(opcode[6:7], 2)
         rs = int(opcode[7:8], 2)
-        # this line below nots an xor
         rd_value = reg_arr[rd]
         rs_value = reg_arr[rs]
         rd_bin = 0
         rs_bin = 0
-        if rd_value < 0:
+        if rd_value < 0:#this if else helps us properly perform 2's complement of a decimal number to binary
             pos_value = 0 - rd_value
             a = 0b1111111111111111 - pos_value + 1
             rd_bin = '{0:016b}'.format(a)
         else:
             rd_bin = '{0:016b}'.format(reg_arr[rd])
 
-        if rs_value < 0:
+        if rs_value < 0:#this also helps us change from deci to bin in 2's Complement
             pos_value = 0 - rs_value
             a = 0b1111111111111111 - pos_value + 1
             rs_bin = '{0:016b}'.format(a)
@@ -166,17 +164,17 @@ def execute_operation(opcode, data_mem, reg_arr, special_reg_arr, pc, branch):
             rs_bin = '{0:016b}'.format(reg_arr[rs])
 
 
-        nxor_value = ~(reg_arr[rd] ^ reg_arr[rs])
+        nxor_value = ~(reg_arr[rd] ^ reg_arr[rs])#this performs the nxor bitwise computation
         nxor_bin = 0
-        if nxor_value < 0:
+        if nxor_value < 0:#this if else helped us debug by seeing what value were computed
             pos_value = 0 - nxor_value
             a = 0b1111111111111111 - pos_value + 1
             nxor_bin = '{0:016b}'.format(a)
         else:
             nxor_bin = '{0:016b}'.format(reg_arr[rd])
 
-        print("RD VALUE: ", rd_value)
-        print("RS VALUE: ", rs_value)
+        print("RD VALUE: ", rd_value)#These print statements helped us view what was going on during the instruction
+        print("RS VALUE: ", rs_value)#They would allow us to debug this instruction more accurately
         print("NXOR VAL: ", nxor_value)
         print("RD BIN:   ", rd_bin)
         print("RS BIN:   ", rs_bin)
@@ -187,6 +185,7 @@ def execute_operation(opcode, data_mem, reg_arr, special_reg_arr, pc, branch):
     elif opcode[1:6] == "11010":
         # EQZ instruction
         # this is the instruction for checking if rd = 0
+        #if rd == 0 then branch = 1
         print("EQZ")
         rd = int(opcode[6:8], 2)
         if reg_arr[rd] == 0:
@@ -244,18 +243,18 @@ def simulator(program_name, instr_mem_file, data_mem_file):
     # Initialize pc and register array
     pc = 0
     branch = 0
-    reg_arr = [0, 0, 0, 0]
-    special_reg_arr = [0, 0, 0, 0]
+    reg_arr = [0, 0, 0, 0]#positions[$R0, $R1, $R2, $R3]
+    special_reg_arr = [0, 0, 0, 0]#positions[$SR0, $SR1, $SR2, $SR3]
 
     print(program_name)
 
     # Create file variables from file name strings
-    instr_mem_input = open(instr_mem_file, "r")
-    data_mem_input = open(data_mem_file, "r")
+    instr_mem_input = open(instr_mem_file, "r")#read file for our programming instructions
+    data_mem_input = open(data_mem_file, "r")#read file for memory on what to compute
 
     instr_mem = file_to_array(instr_mem_input)
     data_mem = file_to_array(data_mem_input)
-    dic = 0
+    dic = 0#at the end of program run we will display the DIC
     while pc < len(instr_mem):
         # data_set is of the following format:
         # [data_mem, reg_arr, special_reg_arr, pc, branch]
@@ -267,18 +266,18 @@ def simulator(program_name, instr_mem_file, data_mem_file):
         special_reg_arr = data_set[2]
         pc = data_set[3]
         branch = data_set[4]
-        print("reg_arr: ",reg_arr)
-        print("special_Reg_arr: ", special_reg_arr)
-        print("MEM[4]: Highest Score ", data_mem[4])
+        print("reg_arr: ",reg_arr)#These lines print for each instruction so we know what is being stored in each register
+        print("special_Reg_arr: ", special_reg_arr)#special register
+        print("MEM[4]: Highest Score ", data_mem[4])#memory locations so we know we're getting the right values stored
         print("MEM[5]: Count         ", data_mem[5])
         print("\n")
-        dic += 1
-        #time.sleep(.001)
+        dic += 1#increment DIC by 1 everytime we perform an instruction
+        #time.sleep(.001)  --we left this at value .2 originally so we could look for bugs in our jump and branch instructions
     print("Dynamic Instruction Count: ", dic)
 
     #print(instr_mem)
     #print(data_mem)
-
+#you can delete the "#" on any of these blocks of code below to see that the code runs correctly, note that both p1 and p2 work
 #simulator("Program 0 : Simulator Testing",
 #          "p3_group_10_p0_imem.txt",
 #          "p3_group_10_dmem_A.txt")
@@ -288,5 +287,5 @@ def simulator(program_name, instr_mem_file, data_mem_file):
 #            "p3_group_10_dmem_B.txt")
 
 simulator("Program 2 : Best Matching Count",
-          "p3_group_10_p2_test.txt",
+          "p3_group_10_p2_imem.txt",
           "p3_group_10_dmem_A.txt")
